e1cb20da6eb7723c5e9660c8c4a2574a806a7c30
yshwaker <yshwaker@gmail.com>
2014-01-19 14:22:56 +0800

    optimized the js code

--- js/script.js
+++ js/script.js
@@ -1,28 +1,9 @@
-// var data = {
-// 	'workTime': null,
-// 	'interval': null
-// };
-var WORKMIN =25;
-var BREAKMIN =5;
-var WORKCOLOR= '#ff9311';
-var BREAKCOLOR = '#87aa1e';
-var currColor = WORKCOLOR;
-var min = WORKMIN;
-var sec = 0;
-//run or pause
-var timerStatus=false;
-//break or work
-var timerType= true;
-var process=0;
-var canvas =$('canvas#timer')[0];
-var ctx = canvas.getContext('2d');
-var width=$(canvas).attr('width');
-
-
-$(document).ready(function(){
-	drawAnime();
-})
-
+var setting = {
+	workmin    : 25,
+	breakmin   : 5,
+	workcolor  : '#ff9311',
+	breakcolor : '#87aa1e'
+};
 
 function setTimeFormat(min,sec)
 {
@@ -31,96 +12,119 @@ function setTimeFormat(min,sec)
 	}else{
 		return min + ":" + sec;
 	}
-
 }
 
-function run()
-{
-	if(min==0&&sec==0){
-		if(timerType==true){
-			$(".break_note").css('display','block');
-			min=BREAKMIN;
-			timerType=false;
-			currColor=BREAKCOLOR;
+var time ={
+	min:setting.workmin,
+	sec:0,
+	status:false, //run or pause
+	timerType:true, //break or work
+	process:0, //percent of passed time
+
+	currentPer:function()
+	{
+		if(this.timerType==true){
+			return (this.min+this.sec/60)/setting.workmin;
 		}else{
-			$(".work_note").css('display','block');
-			min=WORKMIN;
-			timerType=true;
-			currColor=WORKCOLOR;
-			console.log("work");
+			return (this.min+this.sec/60)/setting.breakmin;
 		}
-		$('#ring')[0].play();
-		timerStatus= false;
-		$('#startandpause_btn').text("START");
-		ctx.clearRect(width/2-90,width/2-30,180,60);
-		process=0;
-		drawAnime();
-		return 0; 
+	},
+
+	run:function()
+	{
+		if(this.min==0&&this.sec==0){
+			if(this.timerType==true){
+				$(".break_note").css('display','block');
+				this.min=setting.breakmin;
+				this.timerType=false;
+				myCanvas.currColor=setting.breakcolor;
+				console.log("to break");
+			}else{
+				$(".work_note").css('display','block');
+				this.min=setting.workmin;
+				this.timerType=true;
+				myCanvas.currColor=setting.workcolor;
+				console.log("to work");
+			}
+			$('#ring')[0].play();
+			this.status= false;
+			$('#startandpause_btn').text("START");
+			myCanvas.clearThem(myCanvas.width/2-90,myCanvas.width/2-30,180,60);
+			this.process=0;
+			myCanvas.animate(this.process,setTimeFormat(this.this.min,this.this.sec));
+			return 0; 
+		}
+		if(this.sec==0){
+			this.min=this.min-1;
+		}
+		this.sec = (this.sec+59)%60;
+		myCanvas.clearThem(0,0,myCanvas.width,myCanvas.width);
+		myCanvas.drawArc(0,this.currentPer());
+		myCanvas.drawWord(setTimeFormat(this.min,this.sec));
+		// $('#timer').text(setTimeFormat(this.min,this.sec));
+		this.t=setTimeout('time.run()',1000);
 	}
-	if(sec==0){
-		min=min-1;
-	}
-	sec = (sec+59)%60;
-	drawTimer(setTimeFormat(min,sec));
-	// $('#timer').text(setTimeFormat(min,sec));
-	t=setTimeout('run()',1000);
+
 }
 
-function currentPer()
-{
-	if(timerType==true){
-		return (min+sec/60)/WORKMIN;
-	}else{
-		return (min+sec/60)/BREAKMIN;
+var myCanvas={
+	currColor:setting.workcolor,
+	canvas:$('canvas#timer')[0],
+	ctx : $('canvas#timer')[0].getContext('2d'),
+	width :$($('canvas#timer')[0]).attr('width'),
+
+	drawArc:function(from,to){
+		// ctx.clearRect(0,0,width,width);
+		this.ctx.beginPath();
+		this.ctx.arc(this.width/2,this.width/2,this.width/2-5,2*Math.PI*from,2*Math.PI*to,false);
+		this.ctx.lineWidth=8;
+		this.ctx.strokeStyle = this.currColor;  
+		this.ctx.stroke();  
+		console.log(this.width);
+	},
+
+	clearThem:function(x1,y1,x2,y2){
+		this.ctx.clearRect(x1,y1,x2,y2);
+	},
+
+	drawWord:function(text){
+		this.clearThem(myCanvas.width/2-90,myCanvas.width/2-30,180,60);
+		this.ctx.beginPath();
+		this.ctx.font = "bold 50pt Arial";  
+		this.ctx.fillStyle = '#e33200';  
+		this.ctx.textAlign = 'center';  
+		this.ctx.textBaseline = 'middle';
+		this.ctx.fillText(text, this.width/2, this.width/2);  
+	},
+
+	animate:function(process,text)
+	{
+			this.drawArc(process,process+1/120);
+			process+=(1/130);
+			if(process>1){
+				this.drawWord(text);
+				return;
+			}
+			setTimeout(function(){myCanvas.animate(process,text)},20);
 	}
-
-
 }
 
 
 
+$(document).ready(function(){
+	myCanvas.animate(0,setTimeFormat(time.min,time.sec));
+})
 
-function drawAnime()
-{
-	ctx.beginPath();
-	ctx.arc(width/2,width/2,width/2-5,2*Math.PI*process,2*Math.PI*(process+1/120),false);
-	process+=(1/130);
-	ctx.lineWidth=8;
-	ctx.strokeStyle = currColor;  
-	ctx.stroke();   
-	if(process>1){
-		ctx.font = "bold 50pt Arial";  
-		ctx.fillStyle = '#e33200';  
-		ctx.textAlign = 'center';  
-		ctx.textBaseline = 'middle';  
-		ctx.fillText(setTimeFormat(min,sec), width/2, width/2);
-		return;
-	}
-	setTimeout('drawAnime()',20);
-}
 
-function drawTimer(text)
-{
-	ctx.clearRect(0,0,width,width);
-	ctx.beginPath();
-	// ctx.moveTo(width/2,width/2);
-	ctx.arc(width/2,width/2,width/2-5,0,2*Math.PI*currentPer(),false);
-	ctx.lineWidth=8;
-	ctx.strokeStyle = currColor;  
-	ctx.stroke();  
 
-	ctx.font = "bold 50pt Arial";  
-	ctx.fillStyle = '#e33200';  
-	ctx.textAlign = 'center';  
-	ctx.textBaseline = 'middle';  
-	// ctx.moveTo(width/2, width/2);
-	ctx.fillText(text, width/2, width/2);  
-}
+
+
+
 
 $('#startandpause_btn').click(function(){
 	// if(min==0&&sec==0){
 	// 	if(timerType==true){
-	// 		min=BREAKMIN;
+	// 		min=setting.breakmin;
 	// 		timerType=false;
 	// 	}else{
 
@@ -128,33 +132,33 @@ $('#startandpause_btn').click(function(){
 	$(".break_note").css('display','none');
 	$(".work_note").css('display','none');
 	//}
-	if(timerStatus==true){
-		clearTimeout(t);
+	if(time.status==true){
+		clearTimeout(time.t);
 		$('#startandpause_btn').text("RESUME");
-		timerStatus = false;
+		time.status = false;
+		console.log('is running');
 		return ;
 	}
 	$('#startandpause_btn').text("PAUSE");
-	timerStatus=true;
+	time.status=true;
 	var timerStr = $('#timer');
-	//var timerVal = storage.getworkTime() || WORKMIN;
-	run();
+	//var timerVal = storage.getworkTime() || setting.workmin;
+	time.run();
+	console.log('begin run');
 })
 
 $('#restart_btn').click(function(){
-	process=currentPer();
-	if(timerType==true){
-		min = WORKMIN;
+	time.process=time.currentPer();
+	if(time.timerType==true){
+		time.min = setting.workmin;
 	}else{
-		min = BREAKMIN;
+		time.min = setting.breakmin;
 	}
-	sec = 0;
-	ctx.clearRect(width/2-90,width/2-30,180,60);
-	drawAnime();
+	time.sec = 0;
+	myCanvas.animate(time.process,setTimeFormat(time.min,time.sec));
 	$('#startandpause_btn').text("Start");
-	$('#timer').text(setTimeFormat(min,sec));
-	clearTimeout(t);
-	timerStatus=false;
+	clearTimeout(time.t);
+	time.status=false;
 })
 
 $('#close').click(function(){
